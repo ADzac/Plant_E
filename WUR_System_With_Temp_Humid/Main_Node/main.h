ada382ceaf1154f0fe9c291247210b169e853dc0
ADzac <danielzack786@gmail.com>
2025-06-10 11:42:10 +0200

    Create main.h

---
+++ WUR_System_With_Temp_Humid/Main_Node/main.h
@@ -0,0 +1,27 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Includes ------------------------------------------------------------------*/
+#include "transmission.h"
+#include "stm32wl3x_hal.h"
+#include "stm32wl3x_ll_bus.h"
+#include "stm32wl3x_ll_cortex.h"
+#include "stm32wl3x_ll_rcc.h"
+#include "stm32wl3x_ll_system.h"
+#include "stm32wl3x_ll_utils.h"
+#include "stm32wl3x_ll_gpio.h"
+#include "stm32wl3x_ll_pwr.h"
+#include "stm32wl3x_ll_dma.h"
+#include "stm32wl3x_ll_usart.h"
+#include "stm32wl3x_ll_lpawur.h"
+#include "stm32wl3x_nucleo.h"
+/* Private includes ----------------------------------------------------------*/
+
+void Error_Handler(void);
+#ifdef __cplusplus
+}
+#endif
+#endif /* __MAIN_H */
