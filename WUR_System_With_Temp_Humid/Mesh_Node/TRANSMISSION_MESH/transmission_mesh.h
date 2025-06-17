189505740a0d51be28dc76904d95cfe031b186cf
ADzac <danielzack786@gmail.com>
2025-06-17 12:26:13 +0200

    Update transmission_mesh.h

--- WUR_System_With_Temp_Humid/Mesh_Node/TRANSMISSION_MESH/transmission_mesh.h
+++ WUR_System_With_Temp_Humid/Mesh_Node/TRANSMISSION_MESH/transmission_mesh.h
@@ -21,27 +21,42 @@
 #include "stm32_lpm.h"
 #include "temphumid.h"
 
-#define DISCOVERY 0
-#define DATAREQUEST 1
-#define ALERT 2
+
+#define DISCOVERY_REQ 10
+#define DISCOVERY_RESP 11
+#define ID_ASSIGNMENT 12
+#define DATAREQ 13
+#define DATAREP 14
+
+#define ALERT 15
+
+#define MAIN_NODE_ID 0
+#define UNASSIGNED_ID 0xFF
 
 typedef struct {
     uint8_t TransmissionType;
     uint8_t ID;
     uint8_t Destination;
-    float Temperature;
-    float Humidity;
-    uint8_t Dunno;
-    uint8_t Dunno2;
+    uint8_t Temperature;
+    uint8_t Humidity;
+    uint8_t ADD;
+    uint8_t ID_Assign;
 } Packet;
+
+// Add these function prototypes
+void ProcessDiscoveryPacket(Packet* rxPacket);
+void SendDiscoveryResponse(uint8_t j, uint8_t* vectcTxBuff);
 
 void CreateLPAWURFrameV2(uint8_t j, uint8_t* vectcTxBuff);
 void RandomNumbersGeneration(uint8_t j,uint8_t* vectcTxBuff);
 void MX_APPE_Process(void);
 void UpdateRssiStats(int16_t rssi, int print_stats);
-void GotoRx(uint8_t* m,uint8_t* PR,uint8_t* vectcTxBuff);
+void GotoRx(uint8_t* PR,uint8_t* vectcTxBuff);
 void MX_APPE_Idle(void);
+void GETUID(uint8_t *uid);
 
-
+extern uint8_t Uid[3];
+extern Packet txPacket;
+extern uint8_t vectcTxBuff[15];
 
 #endif
