463922a2699287ca13c39b16d4751494ef8765e0
ADzac <danielzack786@gmail.com>
2025-06-17 10:58:07 +0200

    Update transmission_main.h

--- WUR_System_With_Temp_Humid/Main_Node/TRANSMISSION_MAIN/transmission_main.h
+++ WUR_System_With_Temp_Humid/Main_Node/TRANSMISSION_MAIN/transmission_main.h
@@ -21,19 +21,45 @@
 #include "stm32_lpm.h"
 #include "temphumid.h"
 
-#define DISCOVERY 0
-#define DATAREQUEST 1
-#define ALERT 2
+#define DISCOVERY_REQ 10
+#define DISCOVERY_RESP 11
+#define ID_ASSIGNMENT 12
+#define DATAREQ 13
+#define DATAREP 14
+
+#define ALERT 15
+
+#define MAIN_NODE_ID 0
+#define UNASSIGNED_ID 0xFF
+
+
+#if defined(__ARMCC_VERSION)
+#define UID_WORD0 "0x%X"
+#define UID_WORD1 "0x%X"
+#else
+#define UID_WORD0 "0x%lX"
+#define UID_WORD1 "0x%lX"
+#endif /* defined(__ARMCC_VERSION) */
 
 typedef struct {
     uint8_t TransmissionType;
     uint8_t ID;
     uint8_t Destination;
-    float Temperature;
-    float Humidity;
-    uint8_t Dunno;
-    uint8_t Dunno2;
+    uint8_t Temperature;
+    uint8_t Humidity;
+    uint8_t ADD;
+    uint8_t ID_Assign;
 } Packet;
+
+typedef struct {
+    uint8_t temp;
+    uint8_t hum;
+    uint8_t add;
+    uint8_t id_assign;
+    uint8_t assignedID;
+} NodeEntry;
+
+#define MAX_NODES 50
 
 void CreateLPAWURFrameV2(Packet* packet, uint8_t j, uint8_t* vectcTxBuff);
 void RandomNumbersGeneration(Packet* p,uint8_t j,uint8_t* vectcTxBuff);
@@ -41,7 +67,11 @@ void MX_APPE_Process(void);
 void UpdateRssiStats(int16_t rssi, int print_stats);
 void GotoRx(uint8_t* PR);
 void MX_APPE_Idle(void);
+void GETUID(uint8_t *uid);
+void DiscoveryPhaseHandler(uint8_t ID);
+void AssignID(uint8_t newID);
 
-
+extern Packet txPacket;
+extern Packet rxPacket;
 
 #endif
